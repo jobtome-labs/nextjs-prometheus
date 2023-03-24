4e143e7ba66268ae37b51a3843a26efadc0b1c57
Erik <erik@Eriks-MacBook-Pro.local>
2023-03-24 14:08:16 +0100

    Initial development

---
+++ index.js
@@ -0,0 +1,22 @@
+const instrumentWebFramework = require("./lib/import.js");
+const express = require('express');
+const { client } = require('./lib/prom.js')
+
+instrumentWebFramework(
+  "next/dist/server/next-server",
+  require("./lib/next-server")
+);
+instrumentWebFramework("next/dist/server/render", require("./lib/render"));
+
+
+const app = express();
+const port = process.env.METRICS_PORT || 9091;
+
+app.get('/metrics', async (req, res) => {
+  res.setHeader('Content-Type', client.register.contentType);
+  res.send(await client.register.metrics());
+});
+
+app.listen(port, () => {
+  console.log(`Metrics server is listening on port ${port}`);
+});

232779957035df414e05786e1730fdb5bb4a530d
Szymon Korytnicki <szymon.korytnicki@gmail.com>
2023-05-24 15:20:05 +0200

    Clear metrics after scraping

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const instrumentWebFramework = require("./lib/import.js");
-const express = require('express');
-const { client } = require('./lib/prom.js')
+const express = require("express");
+const { client } = require("./lib/prom.js");
 
-instrumentWebFramework(
-  "next/dist/server/next-server",
-  require("./lib/next-server")
-);
+instrumentWebFramework("next/dist/server/next-server", require("./lib/next-server"));
 instrumentWebFramework("next/dist/server/render", require("./lib/render"));
 instrumentWebFramework("next/dist/server/next-server", require("./lib/middleware"));
-
 
 const app = express();
 const port = process.env.METRICS_PORT || 9091;
 
-app.get('/metrics', async (req, res) => {
-  res.setHeader('Content-Type', client.register.contentType);
+app.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", client.register.contentType);
   res.send(await client.register.metrics());
+  client.register.resetMetrics();
 });
 
 app.listen(port, () => {
